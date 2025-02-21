b04a8bcad7da066ca660c5f07513ee154678b103
GLXY-ARCANE <ahulse1120@gmail.com>
2025-02-20 18:40:24 -0600

    Update on the React update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Header.jsx'
+import Footer from './footer.jsx'
+import Card from './card.jsx'
+
+function App() {
+   
+  return(
+    <>
+      <Header/>
+      <Card />
+      <Footer/>
+    </>
+  );
+}
+
+export default App

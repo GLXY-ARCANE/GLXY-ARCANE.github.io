354d39773050eb071133b1776bb1f24d8ce65bab
GLXY-ARCANE <ahulse1120@gmail.com>
2025-02-21 13:26:46 -0600

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/GLXY-ARCANE.github.io/', // Adjust for your repo
+  base: '/GLXY-ARCANE.github.io/', // Make sure this matches your repo name
+  build: {
+    outDir: 'dist',
+  }
 });
